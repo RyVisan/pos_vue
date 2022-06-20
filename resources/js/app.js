63fea51c8ad12121fe1f0e7882df42fcb5c1db7a
RyVisan <visantry20@gmail.com>
2022-06-20 17:28:41 +0700

    new project pos_vue

---
+++ resources/js/app.js
@@ -0,0 +1,18 @@
+require('./bootstrap');
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+//Router Importd
+import { routes } from './routes';
+
+const router = new VueRouter({
+    routes,
+    mode: 'history'
+})
+
+const app = new Vue({
+    el: '#app',
+    router
+});
