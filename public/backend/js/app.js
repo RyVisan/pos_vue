f170786f110249bab5bd1ff331951bee86acaf9a
RyVisan <visantry20@gmail.com>
2022-06-28 12:04:41 +0700

    solve conflict

--- public/backend/js/app.js
+++ public/backend/js/app.js
@@ -59182,8 +59182,8 @@ var routes = [{
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\DNK-IT-2\Desktop\POS_Vue\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\DNK-IT-2\Desktop\POS_Vue\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\pos_vue\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\pos_vue\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
